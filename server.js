e874385cb36fb4d74b8f1279631909e60c6d3a86
thomascarrollgroup <140523170+thomascarrollgroup@users.noreply.github.com>
2023-08-10 10:07:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Adjust this to match your deployment requirements
